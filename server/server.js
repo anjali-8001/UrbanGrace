43bfd0917a9bfd32f8e871ddf2a9ee3d266488f2
anjali-8001 <anjalisharma8001@gmail.com>
2024-03-14 08:34:03 +0530

    changes in server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 const app = express();
 app.use(
   cors({
-    origin: "https://urbangrace.vercel.app/",
+    origin: "https://urbangrace.vercel.app",
     methods: ["POST", "GET"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
